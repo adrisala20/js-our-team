bc1c01a13da079d1d7490110242e8d362d488e17
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-21 15:42:14 +0100

    add loop for and append in html

--- js/script.js
+++ js/script.js
@@ -20,46 +20,56 @@ let team = [
         'name': 'Wayne',
         'surname': 'Barnett',
         'role': 'Founder & CEO',
-        'images': 'wayne-barnett-founder-ceo.jpg'
+        'img': 'wayne-barnett-founder-ceo.jpg'
     },
     {
         'name': 'Angela',
         'surname': 'Caroll',
         'role': 'Chef Editor',
-        'images': 'angela-caroll-chief-editor.jpg',
+        'img': 'angela-caroll-chief-editor.jpg',
     },
     {
         'name': 'Walter',
         'surname': 'Gordon',
         'role': 'Office Manager',
-        'images':'walter-gordon-office-manager.jpg'
+        'img':'walter-gordon-office-manager.jpg'
     },
     {
         'name': 'Angela',
         'surname': 'Lopez',
         'role': 'Social Media Manager',
-        'images':'angela-lopez-social-media-manager.jpg'
+        'img':'angela-lopez-social-media-manager.jpg'
     },
     {
         'name': 'Scott',
         'surname': 'Estrada',
         'role': 'Developer',
-        'images': 'scott-estrada-developer.jpg'
+        'img': 'scott-estrada-developer.jpg'
     },
     {
         'name': 'Barbara',
         'surname': 'Ramos',
         'role': 'GRaphic Designer',
-        'images': 'barbara-ramos-graphic-designer.jpg'
+        'img': 'barbara-ramos-graphic-designer.jpg'
     }
 ] 
 console.log(team)
 
-const rowEl = document.createElement('div');
-rowEl.classList.add('col');
-console.log(rowEl)
 
 
-rowEl.innerHTML = `
+for(let i = 0 ; i < team.length ;i++){
+    const row = document.querySelector('.row');
+    const rowEl = document.createElement('div');
+    rowEl.classList.add('col');
 
-`;
+    rowEl.innerHTML = `
+    <img src='img/${team[i].img}'> <br>
+    ${team[i].name}
+    ${team[i].surname}<br> 
+    ${team [i].role} <br>
+    `;
+
+    row.appendChild(rowEl)
+    //console.log(rowEl)
+}
+
